cd542f6d60892f65b018ee57ee554a3e99010cc9
hassansys2 <hassansys2@hotmail.com>
2025-07-07 09:40:22 +0500

    Update iOS project structure and add SwiftUI files

---
+++ sample/ios/llvp/llvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  llvpApp.swift
+//  llvp
+//
+//  Created by Hassan Ahmad on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct llvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
